459a195e27bf6d32d39f1797f648ee5d42c4c039
Viacheslav Olshevskyi <viacheslavolshevskyi@gmail.com>
2017-12-10 22:41:34 +0200

    First version

---
+++ FilesHomeTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FilesHomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path: ");
+            string userPath = Console.ReadLine();
+            if (!Directory.Exists(userPath))
+            {
+                Directory.CreateDirectory(userPath);
+            }
+
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Enter text: ");
+
+            string text = Console.ReadLine();
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(userPath+"\\"+fileName, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
